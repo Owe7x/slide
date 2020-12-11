3cfe5abb19dae62dea5149591a3896d0f8d0abc4
Timur <thedickst@gmail.com>
2020-12-11 15:08:31 +0300

    deploy on gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,6 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
   target: 'static',
-  router: {
-      base: '/slide/'
-  },
-  
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'slider',
