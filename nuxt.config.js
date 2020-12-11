2f21f22c568dedbafa02708f78b731d56fb09551
Timur <thedickst@gmail.com>
2020-12-11 17:09:40 +0300

    deploy fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   target: 'static',
+  router: {
+    base: '/slide/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'slider',
