0d219c5af7015b5e1e27152cf053f0b30d4cedec
Timur <thedickst@gmail.com>
2020-12-12 19:38:51 +0300

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,18 @@
 <template>
-  <div class="container">
-    <swiper class="swiper" :options="swiperOption">
-      <swiper-slide class="slide-1"><h2>Titile 1</h2></swiper-slide>
-      <swiper-slide class="slide-2"><h2>Titile 2</h2></swiper-slide>
-      <swiper-slide class="slide-3"><h2>Titile 3</h2></swiper-slide>
-      <swiper-slide class="slide-4"><h2>Titile 4</h2></swiper-slide>
-      <swiper-slide class="slide-5"><h2>Titile 5</h2></swiper-slide>
-  </swiper>
+  <div>
+    <CSlider />
   </div>
 </template>
 
 <script>
 
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
-import 'swiper/swiper-bundle.css';
+import CSlider from '../components/c-slider.vue';
+
 export default {
-    name: 'swiper-example-navigation',
-    title: 'Navigation',
+    name: 'Index',
     components: {
-      Swiper,
-      SwiperSlide
+      CSlider
     },
-    data() {
-      return {
-        swiperOption: {
-          spaceBetween: 30,
-          effect: 'fade',
-        }
-      }
-    }
   }
 </script>
 
@@ -49,20 +33,5 @@ export default {
 swiper-slide {
   background-position: center;
   background-size: cover;
-}
-.slide-1 {
-  background-image: url("../static/images/1.jpg")
-}
-.slide-2 {
-  background-image: url("../static/images/1.jpg")
-}
-.slide-3 {
-  background-image: url("../static/images/1.jpg")
-}
-.slide-4 {
-  background-image: url("../static/images/1.jpg")
-}
-.slide-5 {
-  background-image: url("../static/images/1.jpg")
 }
 </style>
